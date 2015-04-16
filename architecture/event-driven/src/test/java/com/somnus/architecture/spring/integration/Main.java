7fbeadf0a9beaf56c2da7ecef93a7ced7134ad22
zhuyuhua <zhu.yuhua@qq.com>
2015-04-17 00:37:25 +0800

    add by zhuyuhua

--- architecture/event-driven/src/test/java/com/somnus/architecture/spring/integration/Main.java
+++ architecture/event-driven/src/test/java/com/somnus/architecture/spring/integration/Main.java
@@ -7,14 +7,17 @@ public class Main {
 
 	public static void main(String... args) throws Exception {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext(
-				"context.xml");
+				"META-INF/spring/spring-integration-context.xml");
+
 		// Simple Service
 		TempConverter converter = ctx.getBean("simpleGateway",
 				TempConverter.class);
 		System.out.println(converter.fahrenheitToCelcius(68.0f));
+
 		// Web Service
 		converter = ctx.getBean("wsGateway", TempConverter.class);
 		System.out.println(converter.fahrenheitToCelcius(68.0f));
+
 	}
 
 }
