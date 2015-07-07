209ab5609b76b3d4f6138eb6a8ca1948ea2f715d
朱玉华 <zhu.yuhua@qq.com>
2015-07-07 23:46:50 +0800

    update

---
+++ somnus-core/src/main/java/com/somnus/core/wrapper/Pagination.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (c) 2010-2015. Somnus Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.somnus.core.wrapper;
+
+import java.io.Serializable;
+
+/**
+ * 封装页码
+ * 
+ * @author:zhuyuhua
+ * @version 0.0.1
+ */
+public class Pagination<T> implements Serializable {
+
+	private static final long serialVersionUID = -2978759702157110862L;
+
+	/** 总页数 */
+	private int totalPageCount;
+
+	/** 当前页码 */
+	private int pageNo;
+
+	/** 每页总数 */
+	private int pageSize;
+
+	/** 总数据条目数 */
+	private long totalItemCount;
+
+	public Pagination() {
+		this.pageSize = 10;
+		this.pageNo = 0;
+		this.totalPageCount = 1;
+	}
+
+	public static <T> Pagination<T> getInstance(int pageNo, int limit) {
+		if (pageNo < 0) {
+			pageNo = 0;
+		}
+		if (limit <= 0) {
+			limit = 40;
+		}
+		Pagination<T> pagination = new Pagination<T>();
+		pagination.setPageNo(pageNo);
+		pagination.setPageSize(limit);
+		return pagination;
+	}
+
+	public Pagination(Integer pageNo) {
+		this();
+		if (pageNo != null) {
+			this.setPageNo(pageNo);
+		}
+	}
+
+	public Pagination(Integer pageNo, Integer pageSize) {
+		this(pageNo);
+		if (pageSize != null) {
+			this.setPageSize(pageSize);
+		}
+	}
+
+	public void setTotalItemCount(Long itemCount) {
+		if (itemCount != null) {
+			this.totalItemCount = itemCount;
+			this.totalPageCount = (int) ((itemCount + pageSize - 1) / pageSize);
+			this.totalPageCount = totalPageCount <= 0 ? 1 : totalPageCount;
+		}
+	}
+
+	public int getStart() {
+		return pageSize * pageNo;
+	}
+
+	public int getTotalPageCount() {
+		return totalPageCount;
+	}
+
+	public void setTotalPageCount(Integer totalPageCount) {
+		if (totalPageCount != null) {
+			this.totalPageCount = totalPageCount < 1 ? 1 : totalPageCount;
+		}
+	}
+
+	public int getPageNo() {
+		return pageNo;
+	}
+
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(Integer pageSize) {
+		if (pageSize != null) {
+			this.pageSize = pageSize;
+		}
+	}
+
+	public long getTotalItemCount() {
+		return totalItemCount;
+	}
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	/**
+	 * 计算总页数
+	 */
+	public void build() {
+		if (this.pageSize < 0) {
+			this.pageSize = 10;
+		}
+		if (this.totalItemCount < 0) {
+			this.totalItemCount = 0;
+		}
+		this.totalPageCount = (int) ((totalItemCount % pageSize > 0) ? totalItemCount / pageSize + 1
+				: totalItemCount / pageSize);
+	}
+}
---
+++ somnus-utils/src/main/java/com/somnus/utils/bt/ByteUtil.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2010-2015. Somnus Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.somnus.utils.bt;
+
+import java.nio.ByteBuffer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * TODO
+ * 
+ * @author:zhuyuhua
+ * @version 0.0.1
+ */
+public class ByteUtil {
+
+	private static Logger logger = LoggerFactory.getLogger(ByteUtil.class);
+
+	public static byte[] longToByte(long number) {
+
+		byte[] bytes = ByteBuffer.allocate(8).putLong(number).array();
+
+		return bytes;
+
+	}
+
+	// byte数组转成long
+	public static long byteToLong(byte[] b) {
+
+		ByteBuffer bb = ByteBuffer.wrap(b);
+		long l = bb.getLong();
+		return l;
+
+	}
+
+	/**
+	 * 注释：int到字节数组的转换！
+	 * 
+	 * @param number
+	 * @return
+	 */
+	public static byte[] intToByte(int number) {
+		int temp = number;
+		byte[] b = new byte[4];
+		for (int i = 0; i < b.length; i++) {
+			b[i] = new Integer(temp & 0xff).byteValue();//
+			temp = temp >> 8; // 向右移8位
+		}
+		return b;
+	}
+
+	/**
+	 * 注释：字节数组到int的转换！
+	 * 
+	 * @param b
+	 * @return
+	 */
+	public static int byteToInt(byte[] b) {
+		int s = 0;
+		int s0 = b[0] & 0xff;// 最低位
+		int s1 = b[1] & 0xff;
+		int s2 = b[2] & 0xff;
+		int s3 = b[3] & 0xff;
+		s3 <<= 24;
+		s2 <<= 16;
+		s1 <<= 8;
+		s = s0 | s1 | s2 | s3;
+		return s;
+	}
+
+	/**
+	 * 注释：short到字节数组的转换！
+	 * 
+	 * @param s
+	 * @return
+	 */
+	public static byte[] shortToByte(short number) {
+		int temp = number;
+		byte[] b = new byte[2];
+		for (int i = 0; i < b.length; i++) {
+			b[i] = new Integer(temp & 0xff).byteValue();
+			//
+			temp = temp >> 8; // 向右移8位
+		}
+		return b;
+	}
+
+	/**
+	 * 注释：字节数组到short的转换！
+	 * 
+	 * @param b
+	 * @return
+	 */
+	public static short byteToShort(byte[] b) {
+		short s = 0;
+		short s0 = (short) (b[0] & 0xff);// 最低位
+		short s1 = (short) (b[1] & 0xff);
+		s1 <<= 8;
+		s = (short) (s0 | s1);
+		return s;
+	}
+
+}
