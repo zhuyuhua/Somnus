faed9593b489af500a089292041bfc9dcf8327b4
朱玉华 <zhu.yuhua@qq.com>
2015-07-20 19:40:19 +0800

    update

---
+++ somnus-openci/src/main/java/com/somnus/openci/git/GitClientRuntimeException.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2010-2015. Somnus Framework
+ *
+ */
+package com.somnus.openci.git;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * TODO
+ * 
+ * @author zhuyuhua
+ * @version 0.0.1
+ */
+public class GitClientRuntimeException extends RuntimeException {
+
+	private static final long serialVersionUID = 9094674164922576403L;
+
+	private static Logger logger = LoggerFactory.getLogger(GitClientRuntimeException.class);
+
+	public GitClientRuntimeException() {
+		super();
+	}
+
+	public GitClientRuntimeException(Throwable cause) {
+		super(cause);
+	}
+
+	public GitClientRuntimeException(String message) {
+		super(message);
+	}
+
+	public GitClientRuntimeException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
